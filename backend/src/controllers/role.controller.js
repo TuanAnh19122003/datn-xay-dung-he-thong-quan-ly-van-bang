cf7c5a35c05acb459b7302ff6c7e489924111f5d
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-08-29 15:39:00 +0700

    add CRUD APIs for role

---
+++ backend/src/controllers/role.controller.js
@@ -0,0 +1,72 @@
+const RoleService = require('../services/role.service');
+
+class RoleController {
+    async findAll(req, res) {
+        try {
+            const data = await RoleService.findAll();
+            res.status(201).json({
+                message: 'Lấy dữ liệu thành công',
+                data
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const data = await RoleService.create(req.body);
+            res.status(201).json({
+                message: 'Thêm thành công',
+                data
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const data = await RoleService.update(req.params.id, req.body);
+            res.status(201).json({
+                message: 'Cập nhật thành công',
+                data
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            })
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            const deletedCount = await RoleService.delete(req.params.id);
+
+            if (deletedCount === 0) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'Không tìm thấy role để xóa'
+                });
+            }
+
+            res.status(201).json({
+                success: true,
+                message: 'Xóa thành công'
+            });
+        } catch (error) {
+            console.error('Lỗi:', error);
+            res.status(500).json({
+                success: false,
+                message: "Đã xảy ra lỗi khi xóa",
+                error: error.message
+            });
+        }
+    }
+}
+
+module.exports = new RoleController();
--- backend/src/services/role.service.js
+++ backend/src/services/role.service.js
@@ -1,11 +1,26 @@
+const { where } = require('sequelize');
 const Role = require('../models/role.model');
 
-class RoleService{
-    static async findAll(){
-        const data = Role.findAll();
+class RoleService {
+    static async findAll() {
+        const data = await Role.findAll();
         return data
     }
 
+    static async create(data) {
+        const role = await Role.create(data)
+        return role
+    }
+
+    static async update(id, data) {
+        const role = await Role.findOne({ where: { id: id } })
+        if(!role) throw new Error('Không tìm thấy role');
+        return await role.update(data)
+    }
+    
+    static async delete(id) {
+        return await Role.destroy({ where: { id: id } })
+    }
 }
 
-module.exports = RoleService();
+module.exports = RoleService;
