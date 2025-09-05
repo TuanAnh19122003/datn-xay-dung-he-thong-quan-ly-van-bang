7d09fa1f88000ae2cc4c77043022d594b588c0d2
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-09-05 16:52:18 +0700

    add CRUD APIs for major

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const roleRouter = require('./role.routes');
 const userRouter = require('./user.routes');
 const departmentRouter = require('./department.routes');
+const majorRouter = require('./major.routes');
 
 router.use('/roles', roleRouter);
 router.use('/users', userRouter);
 router.use('/departments', departmentRouter);
+router.use('/majors', majorRouter);
 
 module.exports = router;
